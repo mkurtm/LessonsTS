574d97b98b59da7fece103304e46d550922089d4
mkurtm <mkurt@mail.ru>
2017-05-17 16:43:57 +0300

    Решена вторая задача

--- ConsoleApp1/Day 5 Lessons Scratch/Program.cs
+++ ConsoleApp1/Day 5 Lessons Scratch/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Day_5_Lessons_Scratch
@@ -12,10 +13,7 @@ namespace Day_5_Lessons_Scratch
         {
             
 
-
-
-
-
+            
 
 
             Console.ReadLine(); 
@@ -51,6 +49,44 @@ namespace Day_5_Lessons_Scratch
 
             Console.WriteLine("\nНа текущий момент мы обратились к данным {0} раз, и записали {1} раз.", tickCandle.GetCounter, tickCandle.SetCounter);
             Console.WriteLine("\nДемонстрация завершена.");
+        }
+
+        public void Task2()
+        {
+            Candle[] candleArray = new Candle[10];
+            Random rand = new Random();
+
+            Console.WriteLine("Создадим массив. Заполним его 10 экзеплярами Candle. С инициализацией по умолчанию.");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Candle candle = new Candle();
+                candleArray[i] = candle;
+            }
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("\nТеперь заполним все Open в соответствии с условием задачи.");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 0)
+                {
+                    candleArray[0].Open = rand.Next(1, 10);
+                }
+                else
+                {
+                    candleArray[i].Open = candleArray[i - 1].Open + rand.Next(1, 10);
+                }
+            }
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("\nТеперь выведем значения Open всех свечей массива.\n");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Свеча№{0}. Значение Open равно: {1}.", i, candleArray[i].Open);
+            }
         }
     }
 }

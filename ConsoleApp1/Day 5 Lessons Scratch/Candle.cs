eba4483c5922fbef7bbb5ac088d9d7121e3ed51f
mkurtm <mkurt@mail.ru>
2017-05-17 16:11:32 +0300

    Решена первая задача.

---
+++ ConsoleApp1/Day 5 Lessons Scratch/Candle.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5_Lessons_Scratch
+{
+    class Candle
+    {
+        double _open;
+        double _close;
+        double _high;
+        double _low;
+
+        double _volume;
+        DateTime _openDate;
+        DateTime _closeDate;
+
+        public double Open
+        {
+            get
+            {
+                GetCounter++;
+                return _open;
+            }
+            set
+            {
+                _open = value;
+                SetCounter++;
+            }
+        }
+       
+        public double Close
+        {
+            get
+            {
+                GetCounter++;
+                return _close;
+            }
+            set
+            {
+                _close = value;
+                SetCounter++;
+            }
+        }
+
+        public double High
+        {
+            get
+            {
+                GetCounter++;
+                return _high;
+            }
+            set
+            {
+                _high = value;
+                SetCounter++;
+            }
+        }
+
+        public double Low
+        {
+            get
+            {
+                GetCounter++;
+                return _low;
+            }
+            set
+            {
+                _low = value;
+                SetCounter++;
+            }
+        }
+
+        public double Volume
+        {
+            get
+            {
+                GetCounter++;
+                return _volume;
+            }
+            set
+            {
+                _volume = value;
+                SetCounter++;
+            }
+        }
+
+        public DateTime OpenDate
+        {
+            get
+            {
+                GetCounter++;
+                return _openDate;
+            }
+            set
+            {
+                _openDate = value;
+                SetCounter++;
+            }
+        }
+
+        public DateTime CloseDate
+        {
+            get
+            {
+                GetCounter++;
+                return _closeDate;
+            }
+            set
+            {
+                _closeDate = value;
+                SetCounter++;
+            }
+        }
+
+        public ulong GetCounter { get; private set; }
+        public ulong SetCounter { get; private set; }
+
+        public Candle()
+        {
+
+            
+        }
+
+        public Candle(double open, double close, double high,
+                       double low, double volume, DateTime openDate,
+                       DateTime closeDate)
+        {
+            Open = open;
+            Close = close;
+            High = high;
+            Low = low;
+            Volume = volume;
+            OpenDate = openDate;
+            CloseDate = closeDate;
+        }
+
+        protected void GetPlus()
+        {
+            GetCounter++;
+        }
+
+        protected void SetPlus()
+        {
+            SetCounter++;
+        }
+    }
+    
+
+
+    class TickCandle : Candle
+    {
+        int _tickFrame;
+
+        public int TickFrame
+        {
+            get
+            {
+                GetPlus();
+                return _tickFrame;
+            }
+            set
+            {
+                _tickFrame = value;
+                SetPlus();
+            }
+        }        
+
+        public TickCandle(): base()
+        {
+            Open = 1;
+            Close = 1;
+            High = 1;
+            Low = 1;
+            Volume = 1;
+            OpenDate = new DateTime(2001, 1, 1, 0, 0, 0);
+            CloseDate = new DateTime(2001, 1, 1, 0, 0, 0);
+        }
+
+        public TickCandle(double open, double close, double high, double low, double volume, DateTime openDate, DateTime closeDate) : base(open, close, high, low, volume, openDate, closeDate)
+        {
+            
+        }
+
+        public TickCandle(double open, double close, double high, double low) : this ()
+        {
+            Open = open;
+            Close = close;
+            High = high;
+            Low = low;
+        }
+    }
+}
---
+++ ConsoleApp1/Day 5 Lessons Scratch/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5_Lessons_Scratch
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+
+
+
+
+
+
+
+            Console.ReadLine(); 
+
+        }
+
+        public void Task1()
+        {
+            Console.WriteLine("Создадим объект - TickCandle, через базовый конструктор.");
+            TickCandle tickCandle = new TickCandle();
+
+            Console.WriteLine("\nНа текущий момент мы обратились к данным {0} раз, и записали {1} раз.", tickCandle.GetCounter, tickCandle.SetCounter);
+
+            Console.WriteLine("\nТеперь 1 раз запишем и 1 раз обратимся к данным родительского класса.");
+            tickCandle.Open = 10;
+            Console.WriteLine("Мы записали в Open значение 10, а теперь считываем: {0}", tickCandle.Open);
+
+            Console.WriteLine("\nНа текущий момент мы обратились к данным {0} раз, и записали {1} раз.", tickCandle.GetCounter, tickCandle.SetCounter);
+
+            Console.WriteLine("\nТеперь 1 раз запишем и 1 раз обратимся к данным дочернего класса.");
+            tickCandle.TickFrame = 5;
+            Console.WriteLine("Мы записали в TickFrame значение 5, а теперь считываем: {0}", tickCandle.TickFrame);
+
+            Console.WriteLine("\nНа текущий момент мы обратились к данным {0} раз, и записали {1} раз.", tickCandle.GetCounter, tickCandle.SetCounter);
+
+            Console.WriteLine("\nТеперь в цикле 5 раз запишем и 5 раз считаем информацию.");
+            for (int i = 1; i <= 5; i++)
+            {
+                tickCandle.TickFrame = 5;
+                int a = tickCandle.TickFrame;
+                Console.WriteLine("Запись №{0}", i);
+            }
+
+            Console.WriteLine("\nНа текущий момент мы обратились к данным {0} раз, и записали {1} раз.", tickCandle.GetCounter, tickCandle.SetCounter);
+            Console.WriteLine("\nДемонстрация завершена.");
+        }
+    }
+}

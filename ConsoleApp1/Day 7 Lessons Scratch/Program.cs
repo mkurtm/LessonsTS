01f6dd419d38a5627469b85f2e84de6aef86e5fd
mkurtm <mkurt@mail.ru>
2017-05-20 16:52:28 +0300

    Решены задачи

--- ConsoleApp1/Day 7 Lessons Scratch/Program.cs
+++ ConsoleApp1/Day 7 Lessons Scratch/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Day_7_Lessons_Scratch
@@ -10,6 +11,51 @@ namespace Day_7_Lessons_Scratch
     {
         static void Main()
         {
+            Computer comp = new Computer();
+            comp.Launch("WIN 10");
+
+            Console.WriteLine("Создадим объект типа Singletone.\n");
+            Thread.Sleep(1000);
+            Console.WriteLine("На компьютере установлена ОС:{0}.", comp.OS.Name);
+            Thread.Sleep(1000);
+            Console.WriteLine("Попробуем изменить ОС.");
+            Thread.Sleep(1000);
+            comp.Launch("LINUX");
+            Console.WriteLine("На компьютере установлена ОС:{0}.", comp.OS.Name);
+
+
+            Console.ReadKey();
+
+        }
+
+        class Computer
+        {
+            public OS OS { get; set; }
+
+            public void Launch(string osname)
+            {
+                OS = OS.getInstance(osname);
+            }
+        }
+
+
+        class OS
+        {
+            private static OS instance;
+
+            public string Name { get; private set; }
+
+            protected OS(string name)
+            {
+                this.Name = name;
+            }
+
+            public static OS getInstance(string osname)
+            {
+                if (instance == null)
+                    instance = new OS(osname);
+                return instance;
+            }
         }
     }
 }

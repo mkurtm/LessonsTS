16538af7f577bc03b57ee45467046f0385f1f02b
mkurtm <mkurt@mail.ru>
2017-05-23 09:50:09 +0300

    Добавил свой скрипт, начал его наполнять.

---
+++ ConsoleApp1/ConsoleApp1/TSLab scripts/My.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+using TSLab.Script.Helpers;
+
+namespace ConsoleApp1
+{
+    public class My : IExternalScript
+    {
+        public void Execute(IContext ctx, ISecurity sec)
+        {
+            double averageVolume = sec.Volumes.Average();
+            
+
+            var pane = ctx.CreatePane("main", 100, false);
+            var color = new Color(System.Drawing.Color.Blue.ToArgb());
+            var lst = pane.AddList(sec.ToString(), sec, CandleStyles.BAR_CANDLE, color, PaneSides.RIGHT);
+
+            pane = ctx.CreatePane("volume", 10, false);            
+            lst = pane.AddList(sec.ToString(), sec.Volumes, ListStyles.HISTOHRAM, color, LineStyles.SOLID, PaneSides.RIGHT);
+            for (int i = 0; i < ctx.BarsCount; i++)
+                if (sec.Volumes[i] > 5 * averageVolume)
+                    lst.SetColor(i, new Color(System.Drawing.Color.Red.ToArgb()));
+
+
+        }
+        
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/TSLab scripts/MyTradeHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{ 
+    public static class MyTradeHelper
+    {
+        public static double Average (this IList<double> volumes)
+        {
+            double average=0;
+
+            for (int i = 0; i < volumes.Count; i++)
+            {
+                average = (average + volumes[i]);
+            }
+
+            average = average / (volumes.Count+1);
+
+            return average;
+        }
+    }
+}

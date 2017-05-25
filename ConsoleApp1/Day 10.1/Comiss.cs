22a8aaecadee63eae3eae2480d47d7ce42a0314e
mkurtm <mkurt@mail.ru>
2017-05-25 23:43:42 +0300

    Lesson 10.1 Repeat

---
+++ ConsoleApp1/Day 10.1/Comiss.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace Day_10._1
+{
+    public class Comiss : IExternalScript
+    {
+        public void Execute(IContext ctx, ISecurity sec)
+        {
+            //через кубик
+            //var comHnd = new AbsolutCommission() { Commission = 10 };
+            //comHnd.Execute(sec);
+
+            //через АПИ
+            //sec.Commission = (pos, entry) =>
+            //{
+            //    if (entry)
+            //        return 0;
+            //    return pos.Shares * pos.Security.LotSize * 10;
+            //};
+
+            sec.Commission = (pos, entry) =>
+            {
+                if (pos.EntrySignalName == "LE")
+                    return pos.Shares * pos.Security.LotSize * 5 ;
+                return pos.Shares * pos.Security.LotSize * 10;
+            };
+
+
+            for (int i = 0; i < ctx.BarsCount; i++)
+            {
+                var le = sec.Positions.GetLastActiveForSignal("LE");
+                if (i==10)
+                {
+                    sec.Positions.BuyAtMarket(i + 1, 1, "LE");
+                }
+
+                if (i==20)
+                {
+                    le.CloseAtMarket(i + 1, "LX");
+                }
+            }            
+        }
+    }
+}
---
+++ ConsoleApp1/Day 10.1/TradeHelper.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace Day_10._1
+{
+    /// <summary>
+    /// Вспомогательный класс, содержащий хелпер методы. В том числе и методы расширения.
+    /// </summary>
+    public static class TradeHelper
+    {
+        /// <summary>
+        /// Производит вычитание двух коллекций. 
+        /// Из первой вычитает вторую и возвращает коллекцию с элементами равными разности элементов коллекций 1 и 2.
+        /// Если колллекции разной длины, то вернет null.
+        /// </summary>
+        /// <param name="list">Коллекция из которой вычитать</param>
+        /// <param name="subtrList">Колллекция которую будет вычитать.</param>
+        /// <returns></returns>
+        public static IList<double> Subtract(this IList<double> list, IList<double> subtrList)
+        {
+            // Если длины коллекций различаются то просто вернем null как знак ошибки.
+            if (list.Count != subtrList.Count)
+                return null;
+
+            // Создаем массив, и забиваем его разностями элементов списков.
+            var res = new double[list.Count];
+            for (var i = 0; i < list.Count; i++)
+                res[i] = list[i] - subtrList[i];
+
+
+            return res;
+        }
+
+        /// <summary>
+        /// Производит вычитание двух коллекций. 
+        /// Из первой вычитает вторую и возвращает коллекцию с элементами равными разности элементов коллекций 1 и 2.
+        /// Если колллекции разной длины, то вернет null.
+        /// </summary>
+        /// <param name="list">Коллекция из которой вычитать</param>
+        /// <param name="subtrList">Колллекция которую будет вычитать.</param>
+        /// <returns></returns>
+        public static IList<int> Subtract(this IList<int> list, IList<int> subtrList)
+        {
+            // Если длины коллекций различаются то просто вернем null как знак ошибки.
+            if (list.Count != subtrList.Count)
+                return null;
+
+            // Создаем массив, и забиваем его разностями элементов списков.
+            var res = new int[list.Count];
+            for (var i = 0; i < list.Count; i++)
+                res[i] = list[i] - subtrList[i];
+
+
+            return res;
+        }
+
+        /// <summary>
+        /// Производит сложение двух коллекций. 
+        /// К первой коллекции прибавляет вторую и возвращает коллекцию с элементами равными сумме элементов коллекций 1 и 2.
+        /// Если колллекции разной длины, то вернет null.
+        /// </summary>
+        /// <param name="list">Коллекция к которой прибавлять</param>
+        /// <param name="subtrList">Колллекция которую будем прибавлять.</param>
+        /// <returns></returns>
+        public static IList<double> Add(this IList<double> list, IList<double> subtrList)
+        {
+            // Если длины коллекций различаются то просто вернем null как знак ошибки.
+            if (list.Count != subtrList.Count)
+                return null;
+
+            // Создаем массив, и забиваем его суммами элементов списков.
+            var res = new double[list.Count];
+            for (var i = 0; i < list.Count; i++)
+                res[i] = list[i] + subtrList[i];
+
+
+            return res;
+        }
+
+        /// <summary>
+        /// Производит сложение двух коллекций. 
+        /// К первой коллекции прибавляет вторую и возвращает коллекцию с элементами равными сумме элементов коллекций 1 и 2.
+        /// Если колллекции разной длины, то вернет null.
+        /// </summary>
+        /// <param name="list">Коллекция к которой прибавлять</param>
+        /// <param name="subtrList">Колллекция которую будем прибавлять.</param>
+        /// <returns></returns>
+        public static IList<int> Add(this IList<int> list, IList<int> subtrList)
+        {
+            // Если длины коллекций различаются то просто вернем null как знак ошибки.
+            if (list.Count != subtrList.Count)
+                return null;
+
+            // Создаем массив, и забиваем его суммами элементов списков.
+            var res = new int[list.Count];
+            for (var i = 0; i < list.Count; i++)
+                res[i] = list[i] + subtrList[i];
+
+            return res;
+        }
+
+        /// <summary>
+        /// Выводит в лог информационное сообщение.
+        /// </summary>
+        /// <param name="ctx"></param>
+        /// <param name="str"></param>
+        public static void LogInfo(this IContext ctx, string str)
+        {
+            // Задаем цвет соообщения
+            var color = new Color(System.Drawing.Color.Blue.ToArgb());
+
+            ctx.Log(str, color);
+        }
+
+        public static bool IsMaribozu(this Bar candle)
+        {
+            return candle.NoHighShadow() && candle.NoLowShadow();
+        }
+
+        public static bool IsMaribozuWhite(this Bar candle)
+        {
+            return candle.IsWhite() && candle.IsMaribozu();
+        }
+
+        public static bool IsMaribozuBlack(this Bar candle)
+        {
+            return candle.IsBlack() && candle.IsMaribozu();
+        }
+
+        public static bool NoHighShadow(this Bar candle)
+        {
+            if (candle.IsWhite())
+                return candle.High == candle.Close;
+
+            return candle.High == candle.Open;
+        }
+
+        public static bool NoLowShadow(this Bar candle)
+        {
+            if (candle.IsWhite())
+                return candle.Low == candle.Open;
+
+            return candle.Low == candle.Open;
+        }
+
+        public static bool IsWhite(this Bar candle)
+        {
+            return (candle.Close > candle.Open);
+        }
+
+        public static bool IsBlack(this Bar candle)
+        {
+            return (candle.Close > candle.Open);
+        }
+    }
+}
+

604c5d5341ceabec355b1e4385816f39031ce8fd
mkurtm <mkurt@mail.ru>
2017-05-30 13:41:31 +0300

    Lesson 12.Repeat - DONE

---
+++ ConsoleApp1/Day 12 Repeat/QueueLevel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace Day_12_Repeat
+{
+    [HandlerCategory("Marat")]
+    [HandlerName("Уровни стакана")]
+    [HandlerDecimals(3)]
+    public class QueueLevel : IBar2DoubleHandler
+    {
+        private int _level;
+
+        [HandlerParameter(Name = "Уровень", Default = "1")]
+        public int Level
+        {
+            get
+            {
+                return _level;
+            }
+            set
+            {
+                if (value == null)
+                    throw new InvalidOperationException("Уровень не может быть равен 0.");
+                _level = Level;
+            }
+        }
+
+        public IList<double> Execute(ISecurity sec)
+        {
+            var cnt = sec.Bars.Count;
+            var values = new double[cnt];
+
+            //запросим стаканы
+            var buyQ = sec.GetBuyQueue(0);
+            var sellQ = sec.GetSellQueue(0);
+
+            var result = 0.0;
+
+            if (_level > 0 && buyQ.Count >= Math.Abs(_level))     
+                result = buyQ[-Level - 1].Price;
+        
+            if (_level < 0 && buyQ.Count >= Math.Abs(_level))            
+                result = sellQ[Level - 1].Price;            
+    
+            for (int i = 0; i < values.Length; i++)          
+                values[i] = result;
+
+            return values;
+        }
+    }
+}
--- ConsoleApp1/Day 12 Repeat/sma.cs
+++ ConsoleApp1/Day 12 Repeat/sma.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using TSLab.Script.Handlers;
 using TSLab.Script.Helpers;
 

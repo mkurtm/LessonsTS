2d129f2d54757eb08dc8323f380c6f9cd5ce0a25
mkurtm <mkurt@mail.ru>
2017-05-31 21:48:49 +0300

    Repeat DONE

---
+++ ConsoleApp1/Day 13 Repeat/StoreDataGlobalClient.cs
@@ -0,0 +1,16 @@
+﻿using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace Day_13_Repeat
+{
+    public class StoreDataGlobalClient : IExternalScript
+    {
+        public void Execute(IContext ctx, ISecurity sec)
+        {
+            var cache = ctx.LoadGlobalObject("counter");
+            var counter = cache == null ? 0 : (int)cache;          
+            
+            ctx.LogInfo("111   {0}", counter);         
+        }
+    }
+}
---
+++ ConsoleApp1/Day 13 Repeat/StoreDataGlobalHost.cs
@@ -0,0 +1,18 @@
+﻿using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace Day_13_Repeat
+{
+    public class StoreDataGlobalHost : IExternalScript
+    {
+        public void Execute(IContext ctx, ISecurity sec)
+        {
+            var cache = ctx.LoadGlobalObject("counter");
+            var counter = cache == null ? 0 : (int)cache;
+            counter++;
+            
+            //ctx.LogInfo("{0}", counter);
+            ctx.StoreGlobalObject("counter", counter);
+        }
+    }
+}

f9bfd62ad7eb22b229c714e21a9a9530f405acd5
mkurtm <mkurt@mail.ru>
2017-06-01 23:47:22 +0300

    Lesson 14 Repeat Done

---
+++ ConsoleApp1/Day 14 Repeat/ISecRT.cs
@@ -0,0 +1,47 @@
+﻿using TSLab.Script;
+using TSLab.Script.Handlers;
+using TSLab.Script.Realtime;
+using TSLab.DataSource;
+
+namespace Day_14_Repeat
+{
+    public class ISecRT : IExternalScript
+    {
+        public void Execute(IContext ctx, ISecurity sec)
+        {
+            var rtSec = sec as ISecurityRt;
+
+            if (rtSec== null)          
+                ctx.LogInfo("Мы в лабаратории");
+            else
+                ctx.LogInfo("We are at the agent");
+
+            var bid = sec.FinInfo.Bid;
+            var ask = sec.FinInfo.Ask;
+
+            var buyDepo = sec.FinInfo.BuyDeposit;
+            var sellDepo = sec.FinInfo.SellDeposit;
+
+            var prevClose = sec.FinInfo.ClosePrice;
+
+            var upPlank = sec.FinInfo.PriceMax;
+            var downPlank = sec.FinInfo.PriceMin;
+
+            //portfolio, только в режиме агента
+
+            var cb = rtSec.CurrencyBalance; // свободных денег
+            var bc = rtSec.BalanceQuantity; //сколько лотов бумаги в портфеле
+            var eb = rtSec.EstimatedBalance; // чистая стоимость
+            var pn = rtSec.PortfolioName; //имя портфеля
+
+            //как посчитать лоты
+
+            var secPrice = sec.SecurityDescription.ActiveType ==ActiveType.Futures 
+                ? sec.FinInfo.BuyDeposit 
+                : sec.FinInfo.Ask;
+            var posSize = cb / (secPrice * sec.LotSize);
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/Day 14 Repeat/InitDepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace Day_14_Repeat
+{
+    public class InitDepo : IExternalScript
+    {
+        public void Execute(IContext ctx, ISecurity sec)
+        {
+            var depo = sec.InitDeposit;
+
+            ctx.LogInfo("{0}", depo);
+        }
+    }
+}
